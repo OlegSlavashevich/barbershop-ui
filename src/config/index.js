11755bf09cff6ee45081d26e7403480f92c923be
Slavashevich <aslavashevich@ibagroup.eu>
2023-06-27 11:41:55 +0300

    vercel config added

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://barbershop-api-one.vercel.app/';
+export const BASE_URL = 'https://barbershop-ui.vercel.app/';
 
 export const ALLOWED_TIMES = {
   START_TIME: '10:00',
